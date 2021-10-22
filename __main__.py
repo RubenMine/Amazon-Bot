b3987a350cc0f9259499168d022933c635f99270
uomopala <andrea.cosmetico@outlook.it>
2021-10-22 12:08:22 +0200

    Bot Structure

--- __main__.py
+++ __main__.py
@@ -1,13 +1,34 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+import sys
+sys.path.insert(0, 'src')
+from src import bot
 
+from selenium import webdriver
+from bot import AmazonBot
+from bot import Product
+from bot import WebBrowser
+import json
+
+def fetchData():
+    path = "configs\config.json"
+
+    with open(path, "r") as configs:
+        global bot
+        c = json.loads(configs.read())
+
+        auth = c["Auth"]
+        bot = AmazonBot(auth["email"], auth["password"])
+
+        for product in c["Products"]:
+            p = Product(product)
+            bot.addProduct(p)
+    
 
 def main():
-    browser = webdriver.Firefox()
-    browser.get("https://google.com/ncr")
-
+    #browser = webdriver.Firefox()
+    #browser.get("https://google.com/ncr")
+    print(bot)
 
 
 if __name__ == "__main__":
+    fetchData()
     main()
--- src/bot.py
+++ src/bot.py
@@ -1,3 +1,47 @@
-#te puzza il cazzo
+class Product:
 
-#no u
+    def __init__(self, dictProduct) -> None:
+        self.ID = dictProduct["ID"]
+        self.quantity = dictProduct["quantity"]
+        self.price = (float)(dictProduct["price"])
+        self.errorPrice = (float)(dictProduct["errorPrice"])
+
+    def __str__(self):
+        minPrice = self.price - self.errorPrice
+        maxPrice = self.price + self.errorPrice
+        txt = "ID: {} | ".format(self.ID) +\
+               "quantity: {} | ".format(self.quantity) +\
+               "price: {} - {}".format(minPrice, maxPrice)
+        return txt
+
+
+class AmazonBot:
+    
+    def __init__(self, email, pswd) -> None:
+        self.auth = {
+            "email":email,
+            "pswd":pswd,
+        }
+        self.products = []
+
+    def addProduct(self, product):
+        self.products.append(product)
+
+    def checkProducts():
+        pass
+
+    def buyProduct(product:Product):
+        pass
+    
+    def __str__(self):
+        txt = "Email: {}\nPassword: {}".format(self.auth["email"], self.auth["pswd"])
+        for p in self.products:
+            txt += "\n" + str(p)
+        return txt
+
+
+class WebBrowser:
+
+    def __init__(self) -> None:
+        self.url = ""
+        
