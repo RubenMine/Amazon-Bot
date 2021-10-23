7e0d89ec78f114ccdc4f1bf21976922730003bea
uomopala <andrea.cosmetico@outlook.it>
2021-10-23 17:38:27 +0200

    Fixed __str__ methods

--- src/bot.py
+++ src/bot.py
@@ -1,42 +1,47 @@
 class Product:
-    def __init__(self, productsList) -> None:
-        self.ID = productsList["ID"]
-        self.quantity = productsList["quantity"]
-        self.price = (float) (productsList["price"])
-        self.errorPrice = (float) (productsList["errorPrice"])
+
+    def __init__(self, dictProduct) -> None:
+        self.ID = dictProduct["ID"]
+        self.quantity = dictProduct["quantity"]
+        self.price = (float)(dictProduct["price"])
+        self.errorPrice = (float)(dictProduct["errorPrice"])
 
     def __str__(self):
         minPrice = self.price - self.errorPrice
         maxPrice = self.price + self.errorPrice
-        str = "ID: {} | ".format(self.ID) +\
+        txt = "ID: {} | ".format(self.ID) +\
                "quantity: {} | ".format(self.quantity) +\
                "price: {} - {}".format(minPrice, maxPrice)
-        return str
+        return txt
+
 
 class AmazonBot:
-    def __init__(self, email: str, pswd: str) -> None:
+    
+    def __init__(self, email, pswd) -> None:
         self.auth = {
-            "email": email,
-            "pswd": pswd,
+            "email":email,
+            "pswd":pswd,
         }
         self.products = []
 
-    def addProduct(self, product: Product):
+    def addProduct(self, product):
         self.products.append(product)
 
     def checkProducts():
         pass
 
-    def buyProduct(product: Product):
+    def buyProduct(product:Product):
         pass
     
     def __str__(self):
-        str = "Email: {}\nPassword: {}".format(self.auth["email"], self.auth["pswd"])
+        txt = "Email: {}\nPassword: {}".format(self.auth["email"], self.auth["pswd"])
         for p in self.products:
-            str += "\n" + str(p)
-        return str
+            txt += "\n" + str(p)
+        return txt
+
 
 class WebBrowser:
+
     def __init__(self) -> None:
         self.url = ""
         
